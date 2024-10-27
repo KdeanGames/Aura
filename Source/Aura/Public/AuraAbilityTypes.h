3c60a7df867ae4c1470bd6696118e2ce6f36cb4f
Kevin-Dean Livingstone <KevinDean@StarbaseCentral.space>
2024-10-27 09:27:39 -0400

    Implemented Net Serialize

---
+++ Source/Aura/Public/AuraAbilityTypes.h
@@ -0,0 +1,41 @@
+// Copyright © 2024 Kdean Games. All Rights Reserved.
+
+#pragma once
+
+#include "GameplayEffectTypes.h"
+#include "AuraAbilityTypes.generated.h"
+
+
+USTRUCT(BlueprintType)
+struct FAuraGameplayEffectContext : public FGameplayEffectContext
+{
+	GENERATED_BODY()
+
+public:
+
+	bool IsBlockedHit() const { return bIsBlockedHit; }
+	bool IsCritiacalHit() const { return bIsCriticalHit; }
+
+	void SetIsBlockedHit(bool bInIsBlockedHit) { bIsBlockedHit = bInIsBlockedHit; }
+	void SetIsCriticalHit(bool bInIsCriticalHit) { bIsCriticalHit = bInIsCriticalHit; };
+
+
+
+	/** Returns the actual struct used for serialization, subclasses must override this! */
+	virtual UScriptStruct* GetScriptStruct() const
+	{
+		return FGameplayEffectContext::StaticStruct();
+	}
+
+	/** Custom serialization, subclasses must override */
+	virtual bool NetSerialize(FArchive& Ar, class UPackageMap* Map, bool bOutSuccess);
+
+protected:
+	
+	UPROPERTY()
+	bool bIsBlockedHit = false;
+
+	UPROPERTY()
+	bool bIsCriticalHit = false;
+
+};
